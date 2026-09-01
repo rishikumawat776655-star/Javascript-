bde07183877486bdf38958309cd20c70ba9daef8
rishikumawat776655-star <rishikumawat776655@gmail.com>
2026-09-01 19:04:51 +0530

    Update 01_variables.js

--- 01_variables.js
+++ 01_variables.js
@@ -1,3 +1,11 @@
 // console.log("Rishi")
-let score = 33
-console.log(typeof(score))
+// let score = 33
+// console.log(typeof(score))
+// console.log(score)
+
+
+let stringtoNumber="34"
+console.log(typeof(stringtoNumber))
+stringtoNumber = Number(stringtoNumber)
+console.log(typeof(stringtoNumber))
+console.log(stringtoNumber)
